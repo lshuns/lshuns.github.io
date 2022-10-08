6ea5f9138f1416ae1fde3343e8f52509fcd26857
lshuns <1006682997@qq.com>
2022-10-08 11:53:25 +0200

    test talk 6

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,27 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Beijing, China",
+    39.9042,
+    116.4074
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Xiamen, China",
+    24.4795,
+    118.0895
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Leiden, the Netherlands",
+    52.1601,
+    4.4970
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+    "Blankenberge, Belgium",
+    51.3124,
+    3.1327
+  ],
+  [
+    "Sesto, Italy",
+    45.5328,
+    9.2257
+  ]  
 ];
